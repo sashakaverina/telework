e02fc164e9dad344d3e8cc78d2af0e7906b421b5
Sasha Kaverina <sasha.kaverina@lewagon.org>
2022-02-12 15:23:47 +0900

    homescreen

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+    <Stack.Navigator>
+    <Stack.Screen 
+        name='Home' component={HomeScreen}/>
+   </Stack.Navigator>
+  </NavigationContainer>
+);
+
 }
 
 const styles = StyleSheet.create({
